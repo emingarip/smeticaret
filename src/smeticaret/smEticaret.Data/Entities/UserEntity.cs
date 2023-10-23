075b7cc018fe02dfbf93e5af645c24a288608fcf
emingarip <emin@emingarip.com>
2023-10-23 22:51:22 +0300

    Initial commit

---
+++ src/smeticaret/smEticaret.Data/Entities/UserEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smEticaret.Data.Entities
+{
+    public class UserEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public int RoleID { get; set; }
+        public RoleEntity role { get; set; }
+
+    }
+}
---
+++ src/smeticaret/smeticaret.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace smeticaret.mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
